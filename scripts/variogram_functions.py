2438053e14cb303f359787d304296816411372af
pauwiersma <pauwiersma@outlook.com>
2024-04-04 10:04:13 +0000

    Variograms without threads

--- scripts/variogram_functions.py
+++ scripts/variogram_functions.py
@@ -84,6 +84,45 @@ def compare_variograms(obs, simlist, decompose_seasonal =False, gap_indices=None
             gamma_sim_list.append(future.result()[1])
     return bin_center, gamma_obs, gamma_sim_list
 
+def compare_variograms_nothreads(obs, simlist, decompose_seasonal =False, gap_indices=None, bin_number = None):
+    """
+    Compare the experimental variogram of observed data to the variograms of a list of simulated data.
+    ThreadPoolExecutor() is used to parallelize the experimental variogram computatation. 
+
+    Parameters:
+    ----------
+    obs : numpy array
+        Time series of observed data.
+
+    simlist : list of numpy arrays
+        List of time series containing simulated data for comparison.
+
+    decompose_seasonal : bool, optional (default=False)
+        If True, decompose the observed data into seasonal trend and residual components before comparing variograms.
+        Only the residuals are kept for calculation of the variograms. 
+
+    gap_indices : list of int or None, optional (default=None)
+        List of indices corresponding to artifical gaps. If provided, the variograms will only be calculated for these gaps. 
+
+    bin_number : int or None, optional (default=None)
+        Number of bins for variogram calculation. If None, an appropriate number of bins will be determined automatically.
+
+    """
+    if decompose_seasonal==True:
+        if np.count_nonzero(np.isnan(obs))!=0: #decompose function doesn't take nans, so interpolate now and place back the nans later
+            gaps = np.isnan(obs)
+            obs = obs.interpolate('linear')
+        obs_decomposed = sm.tsa.seasonal_decompose(obs, model = 'additive', period = 365)
+        trend = obs_decomposed.trend
+        obs = obs - trend
+        if np.count_nonzero(np.isnan(obs))!=0:
+            obs[gaps] = np.nan 
+        simlist = [sim - trend for sim in simlist] #to make sure they are detrended in the exact same way
+    bin_center, gamma_obs = exp_variogram(obs, gap_indices=gap_indices, bin_number = bin_number)
+    gamma_sim_list = [exp_variogram(sim, gap_indices=gap_indices, bin_number = bin_number)[1] for sim in simlist]    
+    
+    return bin_center, gamma_obs, gamma_sim_list
+
 def plot_variograms(bin_center, gamma_obs,gamma_sim_list,title = None,
                    xlabel = None, ylabel = None):
     f1,ax1 = plt.subplots(figsize = (15,7))
